3ca65e281da34f3a67926b23add33da6615e2f58
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-07-25 13:08:16 +0800

    add: `stretchMove!`, `logAcceptance`

--- src/GW-method.jl
+++ src/GW-method.jl
@@ -5,3 +5,24 @@ sampling of z ~ g(z) ∝ 1/√a, a ∈ [a⁻¹, a]
 """
 @inline sampling(a::Real)          = sampling(a, rand())
 @inline sampling(a::Real, u::Real) = a * u * u - 2 * u * (u - 1) + abs2(u - 1) / a
+
+"""
+    stretchMove!(walker_new::VecI, walker_old::VecI, walker_com::VecI, a::Real)
+
+- `walker_new` := the k-th walker's new vector
+- `walker_old` := the k-th walker's old vector
+- `walker_com` := the randomly chosen walker's vector from complementary ensemble of the k-th walker
+"""
+function stretchMove!(walker_new::VecI, walker_old::VecI, walker_com::VecI, a::Real)
+    z = sampling(a)
+    @simd for i in eachindex(walker_new)
+        @inbounds walker_new[i] = walker_com[i] + z * (walker_old[i] - walker_com[i])
+    end
+    return z
+end
+
+"""
+    logAcceptance(logpr_new::Real, logpr_old::Real, nx::Int, z::Real)
+"""
+@inline logAcceptance(logpr_new::Real, logpr_old::Real, nx::Int, z::Real) =
+    min(0.0, (nx - 1) * log(z) + logpr_new - logpr_old)
