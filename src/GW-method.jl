420c6bb60197c1e7568a4d3d56ab697b566d8147
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-07-25 13:22:29 +0800

    add: `integrateAutocor!`, `estimateBias`, `collect!`

--- src/GW-method.jl
+++ src/GW-method.jl
@@ -112,3 +112,105 @@ function updateMCMC!(walkers_new::MatI, logprs_new::VecI,
     end
     return nothing
 end
+
+"""
+    integrateAutocor!(sampler::MCMCSampler)
+"""
+function integrateAutocor!(sampler::MCMCSampler)
+    chain = sampler.chain
+    ρxsum = sampler.ρxsum
+
+    one2N = axes(chain, 1)
+    one2T = axes(ρxsum, 1)
+    K = size(chain, 2)
+    # Compute autocor. for each walker use view(ρsum, n, 3, :) as buffer
+    # Compute avg. and var. over each walker for each dim. params.
+    for n in one2N, k in 1:K
+        autocor!(view(ρxsum, :, 3, n), view(chain, n, k, :), view(ρxsum, :, 4, n))
+        @inbounds for t in one2T
+            ρxsum[t,1,n], ρxsum[t,2,n] = welfordStep(
+                ρxsum[t,1,n], ρxsum[t,2,n], ρxsum[t,3,n], k
+            )
+        end
+    end
+
+    # Compute Integrated autocor.
+    @inbounds for n in one2N, t in 2:size(ρxsum, 1)
+        ρxsum[t,1,n], ρxsum[t,2,n] = meanVarAdd(
+            1.0, ρxsum[t-1,1,n], ρxsum[t-1,2,n],
+            2.0, ρxsum[t,1,n],   ρxsum[t,2,n]
+        )
+    end
+
+    @inbounds for n in one2N, t in one2T
+        ρxsum[t,1,n], ρxsum[t,2,n] = meanVarDiv(ρxsum[t,1,n], ρxsum[t,2,n], t, 0.0)
+    end
+
+    factor = 2.5 / sqrt(K)
+    @inbounds for n in one2N, t in one2T
+        temp = factor * sqrt(ρxsum[t,2,n])
+        ρxsum[t,3,n] = ρxsum[t,1,n] - temp
+        ρxsum[t,4,n] = ρxsum[t,1,n] + temp
+    end
+
+    return nothing
+end
+
+"""
+    estimateBias(sampler::MCMCSampler; C::Int=5)
+
+Estimate the relaxation time of the discrete autocorrelation time-delay series according to the relation
+
+    M ≥ C ⋅ τf(M)
+
+, where `τf(M)` is the cumulative summation of the autocorrelation time-delay series up to `M` delay.
+"""
+function estimateBias(sampler::MCMCSampler; C::Int=5)
+    ρsum = sampler.ρxsum
+    invC = inv(C)
+
+    ret = 0
+    for n in axes(ρsum, 3)
+        ret = max(ret, estimateBias(view(ρsum, :, 1, n), invC))
+    end
+    return ret
+end
+
+function estimateBias(ρsum::VecI, invC::Real)
+    for i in eachindex(ρsum)
+        if @inbounds ρsum[i] < invC
+            return i
+        end
+    end
+    return length(ρsum)
+end
+
+"""
+    collect!(des::MatIO, sampler::MCMCSampler, bias::Int)
+"""
+function collect!(des::MatIO, sampler::MCMCSampler, bias::Int)
+    chain = sampler.chain
+    collect!(des, view(chain, :, :, bias:size(chain, 3)))
+    return nothing
+end
+
+function collect!(des::MatIO, src::ArrIO{T,3}) where T<:Real
+    cnt = 0
+    for k in axes(src, 2), t in axes(src, 3)
+        cnt += 1
+        @inbounds for n in axes(src, 1)
+            des[n,1], des[n,2] = welfordStep(
+                des[n,1], des[n,2], src[n,k,t], cnt
+            )
+        end
+    end
+
+    fac = 2.5 / sqrt(cnt)
+    @inbounds for n in axes(des, 1)
+        tmp = fac * sqrt(des[n,2])
+        des[n,3] = des[n,1] - tmp
+        des[n,4] = des[n,1] + tmp
+    end
+
+    return nothing
+end
--- src/MCAnalyzer.jl
+++ src/MCAnalyzer.jl
@@ -4,6 +4,8 @@ const VecI  = AbstractVector
 const VecB  = AbstractVector
 const VecIO = AbstractVector
 const MatI  = AbstractMatrix
+const MatIO = AbstractMatrix
+const ArrIO = AbstractArray
 
 include("./utils.jl")
 include("./GW-method.jl")
